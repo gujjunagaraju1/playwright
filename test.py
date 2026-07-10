f23856e3227c2da60e6ba7d1489cf16ecea019c3
gujju <gujjunagaraju19@gmail.com>
2026-07-10 12:38:00 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import Page,expect
+
+def test_login(page:Page):
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",wait_until="domcontentloaded")
+    page.get_by_placeholder("Username").fill("Admin")
+    page.get_by_placeholder("Password").fill("admin123")
+    page.get_by_role("button",name="Login").click()
+    expect(page.get_by_alt_text("client brand banner")).to_be_visible()
+    expect(page.get_by_role("heading",name="Dashboard")).to_be_visible()
+    page.close()
